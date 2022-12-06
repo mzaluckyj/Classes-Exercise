c8f53cc45489ce34fad095cf56cc6c46f0fbe81c
Michael <m.zaluckyj@gmail.com>
2022-12-06 15:46:32 -0500

    excersie 1

---
+++ Classes/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        { }
+        public string make { get; set; }
+        public string model { get; set; }
+        public int year { get; set; }
+        public string Make { get; private set; }
+        public string Model { get; }
+        public int Year { get; }
+
+        //overload method
+        /*public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }   */
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Classes
+﻿using Microsoft.VisualBasic;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myTruck = new Car();
+            {
+                myTruck.make = "Ram";
+                myTruck.model = "1500";
+                myTruck.year = 2021;
+            }
+
+            Car ford = new Car();
+            {
+                ford.make = "Ford";
+                ford.model = "F150";
+                ford.year = 2010;
+            }
+
+            var carlist = new List<Car>() { myTruck, ford,};
+
+            foreach(var vehicle in carlist) { Console.WriteLine($"{vehicle.make} {vehicle.model} {vehicle.year}"); }
         }
+
+
     }
 }
